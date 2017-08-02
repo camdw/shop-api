16988ad5b9dcd427869f6d2582c7a90b6c8210c1
Camille DW <camille.dewulf@gmail.com>
2017-08-02 18:57:16 +0200

    new data organization

--- models/Product.js
+++ models/Product.js
@@ -2,35 +2,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
-	'id': String,
+	'mpn': String,
 	'title': String,
 	'description': String,
-	'mpn': String,
 	'price': Number,
 	'sale_price': Number,
 	'product_type': String,
-  	'color': String,
-	'size': String,
-	'product_image': {
+  	'color': [{
 		'name': String,
-		'path': String
-	},
-	'product_image_2': {
-		'name': String,
-		'path': String
-	},
-	'product_image_3': {
-		'name': String,
-		'path': String
-	},
-	'product_image_4': {
-		'name': String,
-		'path': String
-	},
-	'color_image': {
-		'name': String,
-		'path': String
-	},
+		'code': String
+	  }],
+	'size': [],
 	'related_products': []
 });
 
