ae0f9506cd5012bfdab00c6342c322768dcfec38
Camille DW <camille.dewulf@gmail.com>
2017-08-10 17:44:32 +0200

    modify seeds db

--- bin/seeds-products.js
+++ bin/seeds-products.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/dynamicshop');
+require("dotenv").config();
+
+mongoose.connect(process.env.MONGODB_URI);
 
 const Product = require('../models/Product');
 
